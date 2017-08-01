d24dac4a06369d807fe0b5737901fb8bc55131b4
Nolan Tait <nolanjtait@gmail.com>
2017-07-31 17:53:12 -0700

    Adds specs for command line arguments

--- spec/DocumentSummarizerSpec.js
+++ spec/DocumentSummarizerSpec.js
@@ -2,6 +2,19 @@ describe('DocumentSummarizer', function() {
   const { DocumentSummarizer} = require('../app/DocumentSummarizer.js');
   const MockStream = require('./support/mockstream.js');
 
+  describe('arguments', function() {
+    it('should set default to all flags', function() {
+      summarizer = new DocumentSummarizer;
+      expect(Object.keys(summarizer.argv)).toEqual(['b', 't', 'l', 'g']);
+    });
+
+    it('should set only flags given', function() {
+      summarizer = new DocumentSummarizer({'b': true});
+      expect(Object.keys(summarizer.argv)).toEqual(['b']);
+    });
+    
+  });
+
   describe('data event', function() {
     beforeEach(function() {
       mock = {
@@ -11,16 +24,29 @@ describe('DocumentSummarizer', function() {
         lineGrowthRate: 4
       };
 
-      summarizer = new DocumentSummarizer;
-      stream = new MockStream(mock);
-      stream.pipe(summarizer);
     });
 
     it("should return a loggable message", function(done) {
+      summarizer = new DocumentSummarizer;
+      stream = new MockStream(mock);
+      stream.pipe(summarizer);
+
       summarizer.on('data', (chunk) => {
         expect(chunk.message()).toBeDefined();
         done();
       });
     });
+
+    it('should return only the message for given flags', function(done) {
+      summarizer = new DocumentSummarizer({'b': true});
+      stream = new MockStream(mock);
+      stream.pipe(summarizer);
+
+      summarizer.on('data', (chunk) => {
+        expect(chunk.message()).toBe('Bytes: 1');
+        done();
+      });
+    });
+
   });
 });
